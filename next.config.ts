3f36377fdb969f0dc0a80fa0e603a5a2c81805de
Tommy <tommy.thuy@patroids.com>
2025-04-19 15:22:27 +0700

    appkit demo

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,22 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  serverExternalPackages: ["pino-pretty", "lokijs", "encoding"],
+  webpack: (config, { buildId, dev, isServer, defaultLoaders, nextRuntime, webpack }) => {
+    // ref: https://github.com/WalletConnect/walletconnect-monorepo/issues/1908#issuecomment-1487801131
+    // walletconnect: 'pino-pretty', 'lokijs', 'encoding' are not supported in the browser
+    config.externals = [...config.externals, "pino-pretty", "lokijs", "encoding"];
+
+    return config;
+  },
+  eslint: {
+    // disable linting during build phase
+    ignoreDuringBuilds: true,
+  },
+  typescript: {
+    // disable type-checking during build phase
+    ignoreBuildErrors: true,
+  },
 };
 
 export default nextConfig;
