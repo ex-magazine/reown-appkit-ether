57f03505f6a52f3c20c8fa7662b8a791d5a454db
Tommy <tommy.thuy@patroids.com>
2025-05-01 20:53:24 +0700

    env config

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,14 +1,35 @@
-import { dirname } from 'path';
-import { fileURLToPath } from 'url';
-import { FlatCompat } from '@eslint/eslintrc';
+import { defineConfig } from "eslint/config";
+import path from "node:path";
+import { fileURLToPath } from "node:url";
+import js from "@eslint/js";
+import { FlatCompat } from "@eslint/eslintrc";
+
+// @ts-check
+import prettierConfigRecommended from "eslint-plugin-prettier/recommended";
+import path from "node:path";
+import { fileURLToPath } from "node:url";
+import js from "@eslint/js";
+import { FlatCompat } from "@eslint/eslintrc";
+import { fixupConfigRules } from "@eslint/compat";
+import ts from "typescript-eslint";
+
 
 const __filename = fileURLToPath(import.meta.url);
-const __dirname = dirname(__filename);
-
+const __dirname = path.dirname(__filename);
 const compat = new FlatCompat({
   baseDirectory: __dirname,
+  recommendedConfig: js.configs.recommended,
+  allConfig: js.configs.all
 });
 
-const eslintConfig = [...compat.extends('next/core-web-vitals')];
 
-export default eslintConfig;
+const patchedConfig = fixupConfigRules([...compat.extends("next/core-web-vitals")]);
+
+const config = [
+  ...patchedConfig,
+  ...ts.configs.recommended,
+  prettierConfigRecommended,
+  { ignores: [".next/*"] },
+];
+
+export default config;
