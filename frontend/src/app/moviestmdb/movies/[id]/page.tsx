65e9d033a3885bc2b05b8f76cb1a351aed9d00cc
Tommy <thuy.tranthanh.bd@gmail.com>
2025-05-19 10:13:53 +0700

    movies

--- frontend/src/app/moviestmdb/movies/[id]/page.tsx
+++ frontend/src/app/moviestmdb/movies/[id]/page.tsx
@@ -12,7 +12,8 @@ import {
 import { generateTmdbImagePath } from '@/lib/ether/moviestmdb/tmdb-image-path';
 import RecommendationSection from '@/components/ether/moviestmdb/RecommendationSection';
 import { getImageProps } from 'next/image';
-
+import { getMovieInfo } from '@/utils/movies';
+import { Metadata, ResolvingMetadata } from 'next';
 
 interface Params {
     params: Promise<{ id: string }>;
@@ -20,6 +21,37 @@ interface Params {
 
 export const revalidate = 60;
 
+
+
+
+export async function generateMetadata(
+  { params }: Params,
+  parent: ResolvingMetadata,
+): Promise<Metadata> {
+  const id = (await params).id;
+  const response = await getMovieInfo(id);
+
+  if (!response) return { title: 'Movie not found' };
+
+  return {
+    title: response.title,
+    description: response.overview,
+
+    openGraph: {
+      title: response.title,
+      description: response.overview,
+      images: [
+        {
+          url: `https://image.tmdb.org/t/p/w500/${response.backdrop_path}`,
+          width: 500,
+          height: 750,
+          alt: response.title,
+        },
+      ],
+    },
+  };
+}
+/*
 export const generateStaticParams = async () => {
     const movies = await getMovies(1);
     return movies.map((movie) => ({
@@ -40,7 +72,7 @@ export const generateMetadata = async ({ params }: Params) => {
         image: generateTmdbImagePath(movie.poster_path, 500),
     };
 };
-
+*/
 export default async function MoviePage({ params }: Params) {
     const movieId = (await params).id;
 
@@ -67,9 +99,69 @@ export default async function MoviePage({ params }: Params) {
                     backgroundImage: `linear-gradient(#000000bb, #000000bb), url(${optimizedBackdrop})`,
                 }}
             >
-                sssssssssssssssssss
+                <div className="container flex flex-col md:flex-row gap-8">
+                    <div className="w-full max-w-[200px] max-md:mx-auto md:max-w-[300px]">
+                        <LazyImage
+                            src={generateTmdbImagePath(movie.poster_path, 200)}
+                            alt={movie.title}
+                            width={200}
+                            height={300}
+                            className="rounded-lg border-4 border-border/30"
+                        />
+                    </div>
+                    <div className="w-full md:w-2/3 space-y-4">
+                        <h1 className="text-2xl font-bold">{movie.title}</h1>
+                        <div className="flex items-center gap-2">
+                            <span className="inline-flex gap-2 items-center text-sm">
+                                <FaStar size={14} className="text-orange-500" />
+                                {movie.vote_average.toFixed(1)}
+                            </span>
+
+                            <span className="size-1 bg-gray-500 rounded-full"></span>
+
+                            <span className="text-sm">
+                                {movie.vote_count} votes
+                            </span>
+
+                            <span className="size-1 bg-gray-500 rounded-full"></span>
+
+                            <span className="text-sm">
+                                {dateFormatter(new Date(movie.release_date))}
+                            </span>
+                        </div>
+
+                        <div className="flex gap-2 items-center text-sm !mt-0">
+                            {movie.genres.map((genre, idx) => (
+                                <div key={genre.id}>
+                                    <span
+                                        key={genre.id}
+                                        className="rounded text-sm"
+                                    >
+                                        {genre.name}
+                                    </span>
+                                    {idx < movie.genres.length - 1 && (
+                                        <span className="size-1 bg-gray-500 rounded-full"></span>
+                                    )}
+                                </div>
+                            ))}
+                        </div>
+
+                        <p className="text-sm font-bold">{movie.tagline}</p>
+                        <h5 className="text-xs font-bold uppercase">
+                            Overview
+                        </h5>
+                        <p className="!mt-1">{movie.overview}</p>
+
+                        <AddToWatchlistButton movieId={movie.id} />
+                    </div>
+                </div>
             </section>
-            
+            <div className="container space-y-8">
+                <MovieActorsSection cast={credits.cast} />
+                <RecommendationSection
+                    recommendations={recommendations.results}
+                />
+            </div>
         </>
     );
 }
