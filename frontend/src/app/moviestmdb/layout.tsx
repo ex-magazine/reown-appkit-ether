3cf31fad52f0480464faf2354fbb53e7ca07236c
Tommy <tommy.thuy@patroids.com>
2025-05-19 09:04:53 +0700

    fix movies

--- frontend/src/app/moviestmdb/layout.tsx
+++ frontend/src/app/moviestmdb/layout.tsx
@@ -1,17 +1,30 @@
 import Header from '@/components/ether/moviestmdb/layout/Header';
 import Footer from '@/components/ether/moviestmdb/layout/Footer';
-
+import { ThemeProvider } from '@/components/ether/moviestmdb/layout/ThemeProvider';
 import { Toaster } from 'react-hot-toast';
+
+export const metadata: Metadata = {
+  title: 'Movie Application',
+  description: 'A movie application built with Next.js',
+};
 
 const DashboardLayout = ({ children }: { children: React.ReactNode }) => {
   return (
     <>
+      <ThemeProvider
+                    attribute="class"
+                    defaultTheme="system"
+                    enableSystem
+                    disableTransitionOnChange
+                >
       <Header />
       <main className="min-h-[calc(100vh-150px)]">{children}</main>
       <Toaster position="bottom-center" />
       <Footer />
+      </ThemeProvider>
     </>
   );
 };
 
 export default DashboardLayout;
+
---
+++ frontend/src/components/ether/moviestmdb/layout/ThemeProvider.tsx
@@ -0,0 +1,23 @@
+'use client';
+
+import {
+  ThemeProvider as NextThemesProvider,
+  type ThemeProviderProps,
+} from 'next-themes';
+
+import { useEffect, useState } from 'react';
+
+export function ThemeProvider({ children, ...props }: ThemeProviderProps) {
+    const [isMounted, setIsMounted] = useState<boolean>(false);
+
+    useEffect(() => {
+        setIsMounted(true);
+    }, []);
+
+    if (!isMounted) {
+        return null;
+    }
+
+    return <NextThemesProvider {...props}>{children}</NextThemesProvider>;
+}
+
