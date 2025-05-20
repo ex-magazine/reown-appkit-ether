f899b1e2a7a52a5005f9d2ff5e0419a3dfcb7582
Tommy <thuy.tranthanh.bd@gmail.com>
2025-05-20 09:43:59 +0700

    ether

---
+++ frontend/src/app/oktaycolakoglu/tv/[id]/watch/page.tsx
@@ -0,0 +1,15 @@
+import { MediaWatchProviders } from "@/components/ether/oktaycolakoglu/media-watch-providers"
+
+interface DetailWatchProps {
+  params: {
+    id: string
+  }
+}
+
+export const metadata = {
+  title: "Watch",
+}
+
+export default function DetailWatch({ params }: DetailWatchProps) {
+  return <MediaWatchProviders id={params.id} type="tv" />
+}
